3146f4871b99199804c1e333ed2a2f0baa5f9d13
Sehher <sehher.tariq@academytrainee.com>
2019-09-13 14:24:33 +0100

    added home button

--- GameOhCrud.js
+++ GameOhCrud.js
@@ -7,3 +7,18 @@ function goToUpdate(){
 function goToDelete(){
     location.href = "delete.html";
 }
+
+  
+const req = new XMLHttpRequest();
+req.open('GET', 'http://35.242.168./:9000/gameohcrud');
+req.onload = () => {
+    const data = JSON.parse(req.response);
+    console.log(data)
+    document.getElementById("id").innerHTML = data[0].id;
+    document.getElementById("charName").innerHTML = data[0].character_name;
+    document.getElementById("houseName").innerHTML = data[0].house;
+    document.getElementById("actorName").innerHTML = data[0].actor_name;
+    document.getElementById("alive").innerHTML = data[0].Alive;
+
+}
+req.send();
